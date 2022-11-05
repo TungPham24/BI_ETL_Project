44dd20bab377a252124a72e078dc60a594da0a3a
TungPham24 <tung38085@gmail.com>
2022-11-05 17:56:29 +0700

    lookup iddoanhthu

--- UPDATE_DB.sql
+++ UPDATE_DB.sql
@@ -5,6 +5,10 @@ GO
 
 INSERT INTO DDS (TenBang,NgayCapNhat) VALUES ('Fact', '2011-05-17 22:11:37.423')
 GO
+UPDATE DDS SET NgayCapNhat = '05/17/2011'
+GO
+update Data_Flow SET NgayCapNhat = '05/17/2011'
+GO
 
 USE DDS
 GO
@@ -29,3 +33,25 @@ COMMIT TRAN
 
 GO
 
+USE Stage
+GO
+
+TRUNCATE TABLE KhachHang_Jade_stage
+
+TRUNCATE TABLE CHITIETHOADON_JADE_STAGE
+
+TRUNCATE TABLE HOADON_JADE_STAGE
+
+USE NDS
+GO
+
+DELETE FROM ChiTietHoaDon WHERE NgayCapNhat > '1/1/2020'
+DELETE FROM [dbo].[CuaHang] WHERE NgayCapNhat > '1/1/2020'
+DELETE FROM ChiTietHoaDon WHERE NgayCapNhat > '1/1/2020'
+DELETE FROM [dbo].[HoaDon] WHERE NgayCapNhat > '1/1/2020'
+DELETE FROM [dbo].[KhachHang] WHERE NgayCapNhat > '1/1/2020'
+DELETE FROM [dbo].[LoaiCuaHang] WHERE NgayCapNhat > '1/1/2020'
+DELETE FROM [dbo].[LoaiSanPham] WHERE NgayCapNhat > '1/1/2020'
+DELETE FROM [dbo].[NguonDuLieu] WHERE NgayCapNhat > '1/1/2020'
+DELETE FROM [dbo].[Nuoc] WHERE NgayCapNhat > '1/1/2020'
+DELETE FROM [dbo].[SanPham] WHERE NgayCapNhat > '1/1/2020'
